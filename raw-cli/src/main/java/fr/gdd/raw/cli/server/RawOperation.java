b658e895ee56f50e5c5678f0fad514500f43edd2
theray1 <theray1@outlook.fr>
2024-11-13 09:54:06 +0100

    Server for raw

---
+++ raw-cli/src/main/java/fr/gdd/raw/cli/server/RawOperation.java
@@ -0,0 +1,35 @@
+package fr.gdd.raw.cli.server;
+
+import org.apache.jena.fuseki.FusekiException;
+import org.apache.jena.fuseki.server.FusekiVocab;
+import org.apache.jena.fuseki.server.Operation;
+import org.apache.jena.irix.IRIException;
+import org.apache.jena.irix.IRIx;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+public class RawOperation {
+    private static Model model = ModelFactory.createDefaultModel();
+    public static final Resource opRaw = resource("raw");
+
+    public static final Operation Raw = Operation.alloc(opRaw.asNode(), "raw", "Raw SPARQL Query");
+
+    /* ********************************************************************************* */
+
+    private static Resource resource(String localname) { return model.createResource(iri(localname)); }
+    private static Property property(String localname) { return model.createProperty(iri(localname)); }
+
+    private static String iri(String localname) {
+        String uri = FusekiVocab.NS + localname;
+        try {
+            IRIx iri = IRIx.create(uri);
+            if ( ! iri.isReference() )
+                throw new FusekiException("Bad IRI (relative): "+uri);
+            return uri;
+        } catch (IRIException ex) {
+            throw new FusekiException("Bad IRI: "+uri);
+        }
+    }
+}
